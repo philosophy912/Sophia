237bf4f9781e3ac53748e85f9d3d98c3685a8499
lizhe <lizhe@chinatsp.com>
2020-10-13 14:16:11 +0800

    20201013141610

--- tpl/src/ts.py
+++ tpl/src/ts.py
@@ -7,9 +7,9 @@
 # @Author:      lizhe
 # @Created:     2020/9/24 - 11:06
 # --------------------------------------------------------
-# from automotive import *
-# from src.codes.context import *
-# from time import sleep
+from automotive import *
+from src.codes.context import *
+from time import sleep
 import cv2
 
 # from time import sleep
@@ -39,16 +39,28 @@ import cv2
 # ign_on()
 # sleep(50)
 # close_device()
-avi_file = r"C:\Users\philo\Desktop\2020-10-10_20-20-18.avi"
-cap = cv2.VideoCapture(avi_file)
-while cap.isOpened():
-    ret, frame = cap.read()
+# avi_file = r"C:\Users\philo\Desktop\2020-10-10_20-20-18.avi"
+# cap = cv2.VideoCapture(avi_file)
+# while cap.isOpened():
+#     ret, frame = cap.read()
+#
+#     # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#
+#     cv2.imshow('frame', frame)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+#
+# cap.release()
+# cv2.destroyAllWindows()
 
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+# camera = Camera()
+# camera.open_camera()
+# for i in range(10):
+#     camera.take_picture(rf"d:\temp\video\{Utils.get_time_as_string()}.jpg")
+#     sleep(5)
+#     logger.info(f"the {i + 1} times")
+# camera.close_camera()
+open_device()
+sleep(10)
+fl_doorajarsts_open()
+close_device()
